8e9c6ac46ca5aa5ed5fcfd925ddd04d80fe5c197
Abdulmumin Akinde <lanreywaju97@gmail.com>
2021-09-30 19:11:11 -0700

    refactor code

--- hangman.py
+++ hangman.py
@@ -59,9 +59,14 @@ stages = ['''
 =========
 ''']
 
+end_of_game = False
+wrong_guesses = 0
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 letters_list = list(chosen_word)
+
+# create a "lives" variable
+lives = len(stages)
 
 # Testing code
 print(f"Pssst, the solution is {chosen_word}.")
@@ -81,15 +86,30 @@ print(display)
 # TODO-2: - Loop through each position in the chosen_word;
 # If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 # e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-while "_" in display:
+
+while wrong_guesses < lives - 1:
+    print("\n")
     guess = input("Guess a letter: ").lower()
-    for letter in chosen_word:
-        if letter == guess:
-            letter_index = letters_list.index(guess)
-            display[letter_index] = guess
-            letters_list[letter_index] = "$"
-            print(display)
+    if guess in letters_list:
+        letter_index = letters_list.index(guess)
+        display[letter_index] = guess
+        letters_list[letter_index] = "$"
 
+#TODO-2: - If guess is not a letter in the chosen_word,
+#Then reduce 'lives' by 1. 
+    else:
+      print("\n".join(stages[0:wrong_guesses + 1]))
+      wrong_guesses += 1
+  #If lives goes down to 0 then the game should stop and it should print "You lose."
 
-# TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-# Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+  #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        print("Word completed. Well done!")
+        end_of_game = True
+        print("You win.")
+        break
+
+    #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
