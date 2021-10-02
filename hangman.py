9e01f346caf571e6ece150ca0653c7138b5984bf
Abdulmumin Akinde <lanreywaju97@gmail.com>
2021-10-02 01:30:26 +0100

    create new module. Extensively refactor and optimize code

--- hangman.py
+++ hangman.py
@@ -1,6 +1,5 @@
-# Step 2
-
 import random
+from hangman_words import word_list
 
 stages = ['''
   +---+
@@ -59,54 +58,46 @@ stages = ['''
 =========
 ''']
 
-# hanging_man = stages.reverse()
+
 end_of_game = False
 wrong_guesses = 0
-word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 letters_list = list(chosen_word)
+word_length = len(letters_list)
 
-# create a "lives" variable
-lives = len(stages)
-
-# Testing code
-print(f"Pssst, the solution is {chosen_word}.")
+# create a "lives" variable. This variable will be used to print the status of the hanging man.
+lives = len(stages) - 1
 
 
 # TODO-1: - Create an empty List called display.
 display = []
 # For each letter in the chosen_word, add a "_" to 'display'.
-blanks = "_" * len(chosen_word)
-for space in blanks:
-    display.append(space)
+for _ in range(word_length):
+  display += "_"
 
 print(display)
 # So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
 
 
-# TODO-2: - Loop through each position in the chosen_word;
-# If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-# e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-
-while wrong_guesses < lives - 1:
+while not end_of_game:
     print("\n")
     guess = input("Guess a letter: ").lower()
-    if guess in letters_list:
-        letter_index = letters_list.index(guess)
-        display[letter_index] = guess
+
+    for position in range(word_length):
+      letter = chosen_word[position]
+      if letter in guess:
+          display[position] = guess
         # letters_list[letter_index] = "$"
 
 #TODO-2: - If guess is not a letter in the chosen_word,
 # Increase wrong guesses by 1. Print the ASCII art from 'stages' that corresponds to the current number of wrong guesses the user has made. 
-    else:
-      hanging_stage = stages.pop()
-      print(hanging_stage)
-      # print("\n".join(hanging_stage[0:lives - 1]))
-      wrong_guesses += 1
+    if guess not in chosen_word:
+      lives -= 1
   #If lives goes down to 0 then the game should stop and it should print "You lose."
-    if wrong_guesses == lives:
-      print("You lose")
-      break
+      if lives == 0:
+        end_of_game = True
+        print("You lose")
+        
 
   #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
@@ -116,4 +107,5 @@ while wrong_guesses < lives - 1:
         print("Word completed. Well done!")
         end_of_game = True
         print("You win.")
-        break
+
+    print(stages[lives])
